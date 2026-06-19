6a01b49877b6586dc1e2c646be13670974f7a49b
Desertdustcat <manoetschgi+git@gmail.com>
2026-06-20 01:34:49 +0200

    added basic note for myself (submodules)

---
+++ modules/packages.nix
@@ -0,0 +1,5 @@
+  { config, pkgs, lib, ... }:
+  # {
+  #   imports [ ... ./default.nix ];
+  #   networking.hostName = config.systemnix.hostName;
+  # } # submodles see: https://www.reddit.com/r/NixOS/comments/wzsz4k/comment/im4cf85/
