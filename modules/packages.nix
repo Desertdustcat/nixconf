6b2dfd3fc48ac10c267bc2a21806ef7349c2f2bf
Desertdustcat <manoetschgi+git@gmail.com>
2026-07-03 19:03:22 +0200

    branch changes

--- modules/packages.nix
+++
@@ -1,5 +0,0 @@
-  { config, pkgs, lib, ... }:
-  # {
-  #   imports [ ... ./default.nix ];
-  #   networking.hostName = config.systemnix.hostName;
-  # } # submodles see: https://www.reddit.com/r/NixOS/comments/wzsz4k/comment/im4cf85/
